b96e10b618d1ee28b01dac99eadf1bf0e003861f
= <bradboratto@gmail.com>
2021-01-01 03:51:00 -0500

    tests for multi path support

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 const execSync = require('child_process').execSync;
 const cp = require('child_process')
+const fs = require('fs')
 const colours = require('./colours')
 
-const shell = cmd => cp.execSync(cmd, {'encoding': 'utf-8'})
+const shell = c => cp.execSync(c, {'encoding': 'utf-8'})
+const file = f => fs.readFileSync(f, {'encoding': 'utf8'})
 
 var anyTestFailed = false
 const test = (msg, val, func) => {
@@ -17,7 +19,7 @@ const test = (msg, val, func) => {
         
     } catch(err) {
         log += colours.FgRed + err
-        result = false
+        var result = false
         anyTestFailed = true
     }
     var passFail = result?colours.FgGreen+'test passed':colours.FgRed+'test failed: '+output
@@ -29,16 +31,12 @@ const test = (msg, val, func) => {
 shell('curl -s jsonplaceholder.typicode.com/users > test/usr.json')
 
 test('object root', 'Array[10]\n', 'cat test/usr.json | jxp')
-test('third address', `street: "Hoeger Mall"
-suite: "Apt. 692"
-city: "South Elvis"
-zipcode: "53919-4257"
-geo: Object{lat,lng}
-`, 'cat test/usr.json | jxp 3.address')
+test('third address object', file('test/3adr.txt'), 'cat test/usr.json | jxp 3.address')
 test('third address geo as json', '{"lat":"29.4572","lng":"-164.2990"}\n', 
     'cat test/usr.json | jxp -j 3.address.geo')
 test('throws error on nonexistent value', 'error: could not find key null\n', 
     'cat test/usr.json | jxp -j 3.null')
+test('2-4th users geo (multiple paths)', file('test/24glob.txt'), 'cat test/usr.json | jxp 2.address.geo 3.address.geo 4.address.geo')
 // stest('fail!', 3, ()=>{})
 
 if(!anyTestFailed)
